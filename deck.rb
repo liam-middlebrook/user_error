7e7610ff901e9efb11f7a7534a38d504dec8d173
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-03-31 09:44:34 -0400

    Added initial version of deck

---
+++ deck.rb
@@ -0,0 +1,14 @@
+require 'squib'
+
+Squib::Deck.new(cards: 96, layout: 'layout.yml') do
+  background color: '#EEE'
+  issues = csv file: 'issue_cards.csv'
+  text range: :all, str: issues['Title'], layout: :Title
+  text range: :all, str: issues['Type'], layout: :Type
+  text range: :all, str: issues['StatLabel'], layout: :StatLabel
+  text range: :all, str: issues['Stat'], layout: :Stat
+  text range: :all, str: issues['Special'], layout: :Special
+  text range: :all, str: issues['Flavor'], layout: :Flavor
+
+  save_pdf file: 'test.pdf', gap: 5
+end
